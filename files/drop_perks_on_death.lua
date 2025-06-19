27dbf87be5903bcfdcdfd4f6c21e234f6dd75627
Christian <christian.bankovic@gmail.com>
2025-06-18 22:14:39 -0500

    made this compatible with all kinds of other mods

--- files/drop_perks_on_death.lua
+++ files/drop_perks_on_death.lua
@@ -1,61 +1,24 @@
 dofile_once("data/scripts/lib/utilities.lua")
 dofile_once("data/scripts/perks/perk.lua")
 
--- death = function()
---     local entity_id = GetUpdatedEntityID()
---     local x, y = EntityGetTransform( entity_id )
---     -- GamePrint("Dropping perk!!! from '" .. EntityGetName(entity_id) .. "' at x: " .. x .. " y: " .. y)
---     local vscomps = EntityGetComponent(entity_id, "VariableStorageComponent")
---     for _, vscomp in ipairs(vscomps) do
---         local name = ComponentGetValue2(vscomp, "name")
---         if (name == "perk_id") then
---             local perk_id = ComponentGetValue2(vscomp, "value_string")
---             -- GamePrint("Which perk is this? It's '" .. EntityGetName(perk_id) .. "'")
---             -- GamePrint("Attepting to spawn perk with id '" .. perk_id .. "'")
---             perk_spawn(x, y-10, perk_id, true)
---             -- perk_spawn
---         end
---     end
--- end
-
 ---@type script_death
 death = function()
     local entity_id = GetUpdatedEntityID()
     local x, y = EntityGetTransform( entity_id )
-    GamePrint("Dropping perk!!! from '" .. EntityGetName(entity_id) .. "' at x: " .. x .. " y: " .. y)
+    local perk_ids = {}
+    -- GamePrint("Dropping perk!!! from '" .. EntityGetName(entity_id) .. "' at x: " .. x .. " y: " .. y)
 
-    local perk_icon_entity = EntityGetAllChildren(entity_id,"perk_icon_enemy")[1]
-    if (perk_icon_entity) then
-        local spritecomp = EntityGetComponent(perk_icon_entity, "SpriteComponent")[1]
-        if (spritecomp) then
-            local perk_image_file = ComponentGetValue2(spritecomp, "image_file")
-            if (perk_image_file) then
-                local perk_id = string.upper(string.gsub(string.gsub(perk_image_file,"data/ui_gfx/perk_icons/",""),".png",""))
-                if (perk_id) then
-                    -- GamePrint("Which perk is this? It's '" .. EntityGetName(perk_id) .. "'")
-                    GamePrint("Attepting to spawn perk with id '" .. perk_id .. "'")
-                    perk_spawn(x, y-10, perk_id, true)
-                else
-                    GamePrint("No perk_id")
-                end
-            else
-                GamePrint("No perk_image_file")
-            end
-        else
-            GamePrint("No spritecomp")
+    local vscomps = EntityGetComponent(entity_id, "VariableStorageComponent")
+    for _, vscomp in ipairs(vscomps) do
+        local name = ComponentGetValue2(vscomp, "name")
+        if (name == "perk_id") then
+            local perk_id = ComponentGetValue2(vscomp, "value_string")
+            perk_ids[#perk_ids+1] = perk_id
         end
-    else
-        GamePrint("No perk_icon_entity")
     end
-    
 
-    -- local vscomps = EntityGetComponent(entity_id, "VariableStorageComponent")
-    -- for _, vscomp in ipairs(vscomps) do
-    --     local name = ComponentGetValue2(vscomp, "name")
-    --     if (name == "perk_id") then
-    --         local perk_id = ComponentGetValue2(vscomp, "value_string")
-    --         perk_spawn(x, y-10, perk_id, true)
-    --         -- perk_spawn
-    --     end
-    -- end
+    for i=1, #perk_ids do
+        -- GamePrint("Attepting to spawn perk with id '" .. perk_ids[i] .. "'")
+        perk_spawn(x+(((-1)^(i+1))*10*math.floor(i/2)), y-10, perk_ids[i], true)
+    end
 end
--- files/perk_list_appends.lua
+++ files/perk_list_appends.lua
@@ -1,21 +1,46 @@
-local function tag_entity(entity_id)
-    -- GamePrint("tag_entity() running on '" .. EntityGetName(entity_id) .. "'")
-	EntityAddTag(entity_id, "TAGGED_TO_DROP_PERKS")
-    EntityLoadToEntity( "mods/nightmare_enemies_drop_perks/files/drop_perks_on_death.xml", entity_id )
-    EntityLoadToEntity( "mods/nightmare_enemies_drop_perks/files/colour_true_rainbow.xml", entity_id ) -- For testing purposes
+local function do_perk_tagging_stuff( perk, entity_who_picked )
+    -- print("yo hey this thing got a perk")
+    -- print("perk id: "..perk.id)
+    
+    -- tag_entity(entity_who_picked)
+    
+    if (not EntityHasTag(entity_who_picked, "TAGGED_TO_DROP_PERKS")) then
+        EntityLoadToEntity( "mods/nightmare_enemies_drop_perks/files/drop_perks_on_death.xml", entity_who_picked )
+        -- EntityLoadToEntity( "mods/nightmare_enemies_drop_perks/files/colour_true_rainbow.xml", entity_who_picked ) -- For testing purposes
+        -- print("entity loaded")
+    end
+	EntityAddTag(entity_who_picked, "TAGGED_TO_DROP_PERKS")
+    -- print("tagged")
+    EntityAddComponent(entity_who_picked, "VariableStorageComponent", {
+        name="perk_id",
+        value_string=perk.id
+    })
+    -- print("component added")
 end
 
-for i=1, #perk_list do
-    if (perk_list[i].func_enemy) then
-        local _func_enemy = perk_list[i].func_enemy
-        perk_list[i].func_enemy = function( entity_perk_item, entity_who_picked )
-            tag_entity(entity_who_picked)
-            EntityAddComponent(entity_who_picked, "VariableStorageComponent", {
-                name="perk_id",
-                value_string=entity_perk_item.id
-            })
-            _func_enemy( entity_perk_item, entity_who_picked )
-		end
-
+for i,perk in ipairs(perk_list) do
+    local _func;
+    if (perk.usable_by_enemies) then
+        -- print("perk is usable by enemy!")
+        if (perk.func_enemy ~= nil) then
+            -- print("> perk has func_enemy")
+            _func = perk.func_enemy
+            perk.func_enemy = function( entity_perk_item, entity_who_picked )
+                do_perk_tagging_stuff( perk, entity_who_picked )
+                _func( entity_perk_item, entity_who_picked )
+		    end
+        elseif (perk.func ~= nil) then
+            -- print("> perk has func")
+            _func = perk.func
+            perk.func = function( entity_perk_item, entity_who_picked )
+                do_perk_tagging_stuff( perk, entity_who_picked )
+                _func( entity_perk_item, entity_who_picked )
+		    end
+        else
+            -- print("> perk has NEITHER")
+            perk.func_enemy = function( entity_perk_item, entity_who_picked )
+                do_perk_tagging_stuff( perk, entity_who_picked )
+		    end
+        end
     end
 end
